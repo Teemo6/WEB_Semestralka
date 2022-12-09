4c76fdef27cb0be8e270313d9ab5aa3fa063c9c6
Teemo6 <faragula.v@seznam.cz>
2022-12-09 21:26:07 +0100

    template clanku

--- views/clanky-hlavni.php
+++ views/clanky-hlavni.php
@@ -4,60 +4,66 @@
     <div class="container">
         <h2 class="mt-3">Webová konference na téma programování</h2>
     </div>
-
     <div class="container">
-        <div class="container border border-secondary rounded p-3">
-            <h4>Programování Kruskalovo Algoritmu</h4>
-            <b>Autor: teemor</b>
-            <p>Tak zaprvé, dělal bych to v Pythonu a ne v Javě. Dále, celý operační systém na kterém vyvíjíš je nedostatečný na výkon, obsahuje bloatware a malware typu Windowsák :(. Kdybys používal Arch Linux (jako každá životuschopná bytost), mohl by ses pak lépe zaměřit na svůj problém a pochopit jak nesmyslnou otázku jsi zrovna položil.</p>
-   <div class="col-xs-12">
-                <button type="button" onClick=togglePDF('1') class="btn btn-outline-success m-1 btn-size-110">Zobrazit</button>
-                <button type="button" class="btn btn-outline-success m-1 btn-size-110">*****</button>
-                <button type="button" class="btn btn-success">Ohodnotit</button>
+
+        <div class="container border border-secondary rounded p-3 mt-2">
+            <div class="row">
+                <div class="col-sm-8">
+                    <h4>Programování Kruskalovo Algoritmu</h4>
+                    <p class="lead"><i class="fa fa-user"></i> SuperAdmin</p>
+                </div>
+                <div class="col-sm-4">
+                    <button type="button" onClick=togglePDF('1') class="btn btn-outline-success m-1 pull-right">Zobrazit</button>
+                </div>
             </div>
-            <iframe src="<?= DIR_UTILITY ?>/pdf/Zadani_PT.pdf" id="PDF_1" width="100%" height="400px" style="display:none"></iframe>
-        </div>
-
-        <div class="container border border-secondary rounded p-3">
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer tempor. Fusce aliquam vestibulum ipsum. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. Aenean id metus id velit ullamcorper pulvinar. Morbi scelerisque luctus velit. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Phasellus rhoncus. Nullam sit amet magna in magna gravida vehicula. Aliquam id dolor. Integer pellentesque quam vel velit. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.</p>
-            <div class="col-xs-12">
-                <button type="button" onClick=togglePDF('2') class="btn btn-outline-success m-1 btn-size-110">Zobrazit</button>
-                <button type="button" class="btn btn-outline-success m-1 btn-size-110">*****</button>
-                <button type="button" class="btn btn-success">Ohodnotit</button>
-            </div>
-            <iframe src="<?= DIR_UTILITY ?>/pdf/Zadani_PT.pdf" id="PDF_2" width="100%" height="500px" style="display:none"></iframe>
-
-        </div>
-
-        <div class="container border border-secondary rounded p-3">
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer tempor. Fusce aliquam vestibulum ipsum. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. Aenean id metus id velit ullamcorper pulvinar. Morbi scelerisque luctus velit. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Phasellus rhoncus. Nullam sit amet magna in magna gravida vehicula. Aliquam id dolor. Integer pellentesque quam vel velit. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.</p>
-            <div class="col-xs-12">
-                <button type="button" class="btn btn-outline-success m-1 btn-size-110">*****</button>
-                <button type="button" class="btn btn-success">Ohodnotit</button>
+            <div>
+                <p>Tak zaprvé, dělal bych to v Pythonu a ne v Javě. Dále, celý operační systém na kterém vyvíjíš je nedostatečný na výkon, obsahuje bloatware a malware typu Windowsák :(. Kdybys používal Arch Linux (jako každá životuschopná bytost), mohl by ses pak lépe zaměřit na svůj problém a pochopit jak nesmyslnou otázku jsi zrovna položil.</p>
+                <iframe src="<?= DIR_UTILITY ?>/pdf/Zadani_PT.pdf" id="PDF_1" width="100%" height="400px" style="display:none"></iframe>
             </div>
         </div>
-
-        <div class="container border border-secondary rounded p-3">
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer tempor. Fusce aliquam vestibulum ipsum. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. Aenean id metus id velit ullamcorper pulvinar. Morbi scelerisque luctus velit. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Phasellus rhoncus. Nullam sit amet magna in magna gravida vehicula. Aliquam id dolor. Integer pellentesque quam vel velit. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.</p>
-            <div class="col-xs-12">
-                <button type="button" class="btn btn-outline-success m-1 btn-size-110">*****</button>
-                <button type="button" class="btn btn-success">Ohodnotit</button>
+        <div class="container border border-secondary rounded p-3 mt-2">
+            <div class="row">
+                <div class="col-sm-8">
+                    <h4>Programování Kruskalovo Algoritmu</h4>
+                    <p class="lead"><i class="fa fa-user"></i> SuperAdmin</p>
+                </div>
+                <div class="col-sm-4">
+                    <button type="button" onClick=togglePDF('2') class="btn btn-outline-success m-1 pull-right">Zobrazit</button>
+                </div>
+            </div>
+            <div>
+                <p>Tak zaprvé, dělal bych to v Pythonu a ne v Javě. Dále, celý operační systém na kterém vyvíjíš je nedostatečný na výkon, obsahuje bloatware a malware typu Windowsák :(. Kdybys používal Arch Linux (jako každá životuschopná bytost), mohl by ses pak lépe zaměřit na svůj problém a pochopit jak nesmyslnou otázku jsi zrovna položil.</p>
+                <iframe src="<?= DIR_UTILITY ?>/pdf/Zadani_PT.pdf" id="PDF_2" width="100%" height="400px" style="display:none"></iframe>
             </div>
         </div>
-
-        <div class="container border border-secondary rounded p-3">
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer tempor. Fusce aliquam vestibulum ipsum. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. Aenean id metus id velit ullamcorper pulvinar. Morbi scelerisque luctus velit. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Phasellus rhoncus. Nullam sit amet magna in magna gravida vehicula. Aliquam id dolor. Integer pellentesque quam vel velit. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.</p>
-            <div class="col-xs-12">
-                <button type="button" class="btn btn-outline-success m-1 btn-size-110">*****</button>
-                <button type="button" class="btn btn-success">Ohodnotit</button>
+        <div class="container border border-secondary rounded p-3 mt-2">
+            <div class="row">
+                <div class="col-sm-8">
+                    <h4>Programování Kruskalovo Algoritmu</h4>
+                    <p class="lead"><i class="fa fa-user"></i> SuperAdmin</p>
+                </div>
+                <div class="col-sm-4">
+                    <button type="button" onClick=togglePDF('3') class="btn btn-outline-success m-1 pull-right">Zobrazit</button>
+                </div>
+            </div>
+            <div>
+                <p>Tak zaprvé, dělal bych to v Pythonu a ne v Javě. Dále, celý operační systém na kterém vyvíjíš je nedostatečný na výkon, obsahuje bloatware a malware typu Windowsák :(. Kdybys používal Arch Linux (jako každá životuschopná bytost), mohl by ses pak lépe zaměřit na svůj problém a pochopit jak nesmyslnou otázku jsi zrovna položil.</p>
+                <iframe src="<?= DIR_UTILITY ?>/pdf/Zadani_PT.pdf" id="PDF_3" width="100%" height="400px" style="display:none"></iframe>
             </div>
         </div>
-
-        <div class="container border border-secondary rounded p-3">
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer tempor. Fusce aliquam vestibulum ipsum. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. Aenean id metus id velit ullamcorper pulvinar. Morbi scelerisque luctus velit. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Phasellus rhoncus. Nullam sit amet magna in magna gravida vehicula. Aliquam id dolor. Integer pellentesque quam vel velit. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.</p>
-            <div class="col-xs-12">
-                <button type="button" class="btn btn-outline-success m-1 btn-size-110">*****</button>
-                <button type="button" class="btn btn-success">Ohodnotit</button>
+        <div class="container border border-secondary rounded p-3 mt-2">
+            <div class="row">
+                <div class="col-sm-8">
+                    <h4>Programování Kruskalovo Algoritmu</h4>
+                    <p class="lead"><i class="fa fa-user"></i> SuperAdmin</p>
+                </div>
+                <div class="col-sm-4">
+                    <button type="button" onClick=togglePDF('4') class="btn btn-outline-success m-1 pull-right">Zobrazit</button>
+                </div>
+            </div>
+            <div>
+                <p>Tak zaprvé, dělal bych to v Pythonu a ne v Javě. Dále, celý operační systém na kterém vyvíjíš je nedostatečný na výkon, obsahuje bloatware a malware typu Windowsák :(. Kdybys používal Arch Linux (jako každá životuschopná bytost), mohl by ses pak lépe zaměřit na svůj problém a pochopit jak nesmyslnou otázku jsi zrovna položil.</p>
+                <iframe src="<?= DIR_UTILITY ?>/pdf/Zadani_PT.pdf" id="PDF_4" width="100%" height="400px" style="display:none"></iframe>
             </div>
         </div>
     </div>
